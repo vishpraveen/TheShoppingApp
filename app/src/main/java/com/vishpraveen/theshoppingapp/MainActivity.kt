d8e77d88600cf322b8d8ca71514049caef5f0b94
vishpraveen <vishpraveen007@gmail.com>
2023-04-05 12:25:18 +0530

    Initial Commit

---
+++ app/src/main/java/com/vishpraveen/theshoppingapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.vishpraveen.theshoppingapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vishpraveen.theshoppingapp.ui.theme.TheShoppingAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TheShoppingAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TheShoppingAppTheme {
+        Greeting("Android")
+    }
+}
