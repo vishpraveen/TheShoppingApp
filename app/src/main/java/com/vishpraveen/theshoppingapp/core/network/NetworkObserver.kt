e3312395a9f6e1b56c5ea46f5890e0cafc35a33b
vishpraveen <vishpraveen007@gmail.com>
2023-11-18 14:50:17 +0530

    updated NetworkObserver

--- app/src/main/java/com/vishpraveen/theshoppingapp/core/network/NetworkObserver.kt
+++ app/src/main/java/com/vishpraveen/theshoppingapp/core/network/NetworkObserver.kt
@@ -1,40 +1,7 @@
 package com.vishpraveen.theshoppingapp.core.network
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.Network
-import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
-import android.net.NetworkCapabilities.TRANSPORT_CELLULAR
-import android.net.NetworkCapabilities.TRANSPORT_WIFI
-import android.net.NetworkRequest
-import android.util.Log
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 
-class NetworkObserver(context: Context) {
-    private val connectivityManager: ConnectivityManager = context.getSystemService(ConnectivityManager::class.java)
-    private val TAG = this::class.simpleName
-
-    fun connectionStatus(): Flow<Boolean> = callbackFlow{
-        val networkCallback = object: ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                Log.e(TAG, "onAvailable: available")
-                trySend(true)
-            }
-
-            override fun onLost(network: Network) {
-                Log.e(TAG, "onLost: lost", )
-                trySend(false)
-            }
-        }
-        connectivityManager.registerNetworkCallback(
-            NetworkRequest.Builder().addCapability(NET_CAPABILITY_INTERNET)
-                .addTransportType(TRANSPORT_WIFI)
-                .addTransportType(TRANSPORT_CELLULAR)
-                .build(),
-            networkCallback
-        )
-        awaitClose { connectivityManager.unregisterNetworkCallback(networkCallback) }
-    }
+interface NetworkObserver {
+    fun connectionStatus(): Flow<Boolean>
 }
